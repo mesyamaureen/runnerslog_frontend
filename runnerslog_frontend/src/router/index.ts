db70e734f8e06adfcf76df1ad047e058eb4550e1
mesyamaureen <mesyamaureen@gmail.com>
2023-11-15 17:59:12 +0100

    files in components created

--- runnerslog_frontend/src/router/index.ts
+++ runnerslog_frontend/src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import HelloWorldVue from '@/components/HelloWorld.vue'
+import HelloWorldVue from '@/components/startseite/HelloWorld.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
